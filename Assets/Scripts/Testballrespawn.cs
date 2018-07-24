04d0a69bfec081eab9f7e99f48d8ec51f1ad10d7
Unknown <j.culig@yahoo.de>
2018-07-24 19:21:21 +0200

    Joystick + Camera First Try
    Joystick implementiert. Erster Versuch Camera

---
+++ Assets/Scripts/Testballrespawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Testballrespawn : MonoBehaviour {
+    private Vector3 respawn;
+	// Use this for initialization
+	void Start () {
+        respawn = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.position.y <= 0)
+        {
+            transform.position = respawn;
+        }
+    }
+}
---
+++ Assets/Scripts/VirtualJoystick.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+//Inspired by https://www.youtube.com/watch?v=uSnZuBhOA2U
+
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    public Image backgroundImg, joystickImg;
+    public Vector3 InputDirection { set; get; }
+
+
+    private void Start()
+    {
+        backgroundImg = GetComponent<Image>();
+        joystickImg = transform.GetChild(0).GetComponent<Image>();
+        InputDirection = Vector3.zero;
+
+        
+    }
+
+    //EventSystems interfaces
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Vector2 pos = Vector2.zero;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle
+            (backgroundImg.rectTransform,
+                ped.position,
+                ped.pressEventCamera,
+                out pos))
+        {
+
+            pos.x = (pos.x / backgroundImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / backgroundImg.rectTransform.sizeDelta.y);
+
+            InputDirection = new Vector3(pos.x * 2 - 1, 0, pos.y * 2 - 1);
+
+            if (InputDirection.magnitude > 1.0f)
+            {
+                InputDirection = InputDirection.normalized;
+            }
+
+            Debug.Log(InputDirection);
+
+            //Move Joystick
+
+            joystickImg.rectTransform.anchoredPosition = new Vector3(InputDirection.x * (int)(backgroundImg.rectTransform.sizeDelta.x/2.5),
+                                                                     InputDirection.z * (int)(backgroundImg.rectTransform.sizeDelta.y/2.5));
+        }
+    }
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        //Resset Joystickposition if dropped
+        InputDirection = Vector3.zero;
+        //InputDirection = Vector3.zero;
+        joystickImg.rectTransform.anchoredPosition = Vector3.zero;      
+    }
+
+}
